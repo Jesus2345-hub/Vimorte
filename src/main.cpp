93b87a6f280f97e462d3bda419c478a35e20eb94
Jesús Fernández <jesus123ffn@gmail.com>
2026-03-15 08:39:11 -0400

    feat: integrar SFML en Vimorte

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hola Vimorte!" << std::endl;
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Vimorte");
+
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
