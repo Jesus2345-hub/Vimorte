c249f8d1fc7c2e68bbfa6ddd9df9cd34c3a48c10
Jesús Fernández <jesus123ffn@gmail.com>
2026-03-29 01:27:20 -0400

    feat: agregar target_include_directories a CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include "test/Prueba.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Vimorte");
-
+    Prueba prueba;
+    prueba.saludar();
+    
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Vimorte");
+    
     sf::CircleShape shape(50.f);
     shape.setFillColor(sf::Color::Green);
-
+    
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
+        
         window.clear();
         window.draw(shape);
         window.display();
     }
-
+    
     return 0;
 }
